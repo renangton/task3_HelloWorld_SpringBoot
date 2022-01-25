fe8900731275cf55dac14e0921e13576aad3d8ac
renangton <puccho0427@gmail.com>
2022-01-25 22:02:18 +0900

    コメントを追加

--- src/main/java/com/HelloWorld/HelloController.java
+++ src/main/java/com/HelloWorld/HelloController.java
@@ -7,17 +7,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+// HTTPリクエストを受け付ける
 @Controller
 public class HelloController{
 
+	//　[http://localhost:8080/hello-template]へGETメソットでのHTTPリクエストを受け付ける	
 	@GetMapping("/hello-template")
 	public String hello(Model model) {
+		// message:「I am Renato」をビューに渡す
 		model.addAttribute("message", "I am Renato");
 		
+		// 現在時刻を所得し、フォーマットに変換
 		LocalDateTime today = LocalDateTime.now();
 		DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 		String timeDisp = today.format(timeFormat);
+		
+		// now:timeDisp(現在時刻）をビューに渡す
 		model.addAttribute("now", timeDisp);
+		
+		//　HelloWorld_SpringBoot.htmlに画面遷移		
 		return "HelloWorld_SpringBoot";
 	}
 }
